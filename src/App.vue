81bd43ba0098a18a79bce3d3aea0b7545e780b60
sourav <jangidsourav97@gmail.com>
2024-10-06 08:00:28 +0530

    d

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import HomeView from './views/HomeView.vue'
+import HomeView from "./views/HomeView.vue";
 </script>
 <template>
-<HomeView/>
+  <HomeView />
 </template>
-
